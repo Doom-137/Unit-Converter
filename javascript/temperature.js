f0ec1b5a2762796a3ae4e443e0fb5019e5ca63f1
Michael Gross <96743493+Doom-137@users.noreply.github.com>
2022-01-11 15:06:00 -0500

    Update temperature.js

--- javascript/temperature.js
+++ javascript/temperature.js
@@ -1,10 +1,73 @@
 // Converts temperature units between Farenheit 'F', celcius 'C', Reaumur 'R', Kelvin 'K', Newton 'N', Delisle 'DE', Romer 'RO'
-function convertTemp (input, inputUnit = 'F', outputUnit = 'C') {
+function getTemp (input, code) {
+	const conversionEqs = {
+		'cde': function () { return (100 - input) * 3/2; },
+		'cf': function () { return (input * 9/5) + 32; },
+		'ck': function () { return input + 273.15; },
+		'cn': function () { return input * 33/100; },
+		'cr': function () { return (input + 273.15) * 9/5; },
+		'cre': function () { return input * 4/5; },
+		'cro': function () { return input * 21/40 + 7.5; },
+		'dec': function () { return 100 - input * 2/3; },
+		'def': function () { return 212 - input * 6/5; },
+		'dek': function () { return 373.15 - input * 2/3; },
+		'den': function () { return 33 - input * 11/50; },
+		'der': function () { return 671.67 - input * 6/5; },
+		'dere': function () { return 80 - input * 8/15; },
+		'dero': function () { return 60 - input * 7/20; },
+		'fc': function () { return (input - 32) * 5/9; },
+		'fde': function () { return (212 - input) * 5/6; },
+		'fk': function () { return (input + 459.67) * 5/9; },
+		'fn': function () { return (input - 32) * 11/60; },
+		'fr': function () { return input + 459.67; },
+		'fre': function () { return (input - 32) * 4/9; },
+		'fro': function () { return (input - 32) * 7/24 + 7.5; },
+		'kc': function () { return input - 273.15; },
+		'kde': function () { return (373.15 - input) * 3/2; },
+		'kf': function () { return input * 9/5 - 459.67; },
+		'kn': function () { return (input - 273.15) * 33/100; },
+		'kr': function () { return input * 9/5; },
+		'kre': function () { return (input - 273.15) * 4/5; },
+		'kro': function () { return (input - 273.15) * 21/40 + 7.5; },
+		'nc': function () { return input * 100/33; },
+		'nde': function () { return (33 - input) * 50/11; },
+		'nf': function () { return input * 60/11 + 32; },
+		'nk': function () { return input * 100/33 + 273.15; },
+		'nr': function () { return input * 60/11 + 491.67; },
+		'nre': function () { return input * 80/33; },
+		'nro': function () { return input * 35/22 + 7.5; },
+		'rc': function () { return (input - 491.67) * 5/9; },
+		'rde': function () { return (671.67 - input) * 5/6; },
+		'rf': function () { return input - 459.67; },
+		'rk': function () { return input * 5/9; },
+		'rn': function () { return (input - 491.67) * 11/60; },
+		'rre': function () { return (input - 491.67) * 4/9; },
+		'rro': function () { return (input - 491.67) * 7/24 + 7.5; },
+		'rec': function () { return input * 5/4; },
+		'rede': function () { return (80 - input) * 15/8; },
+		'ref': function () { return input * 9/4 + 32; },
+		'rek': function () { return input * 5/4 + 273.15; },
+		'ren': function () { return input * 33/80; },
+		'rer': function () { return input * 9/4 + 491.67; },
+		'rero': function () { return input * 21/32 + 7.5; },
+		'roc': function () { return (input - 7.5) * 40/21; },
+		'rode': function () { return (60 - input) * 20/7; },
+		'rof': function () { return (input - 7.5) * 24/7 + 32; },
+		'rok': function () { return (input - 7.5) * 40/21 + 273.15; },
+		'ron': function () { return (input - 7.5) * 22/35; },
+		'ror': function () { return (input - 7.5) * 24/7 + 491.67; },
+		'rore': function () { return (input - 7.5) * 32/21; }
+	};
+	return conversionEqs[code]();
+}
+
+function convertTemp (input, inputUnit = 'f', outputUnit = 'c') {
 	const errorMessage = 'Unit(s) invalid, please check entered input and output units and try again.';
-	const unitsArr = ['C', 'DE', 'F', 'K', 'N', 'R', 'RE', 'RO'];
+	const unitsArr = ['c', 'de', 'f', 'k', 'n', 'r', 're', 'ro'];
 	let validator = 0;
-	const outputU = outputUnit.toUpperCase();
-	const inputU = inputUnit.toUpperCase();
+	const inputU = inputUnit.toLowerCase();
+	const outputU = outputUnit.toLowerCase();
+	const convertCode = inputU + outputU;
 	
 	for (let i = 0; i < unitsArr.length; i++) {
 		if (unitsArr[i] === inputU || unitsArr[i] === outputU) { validator++; }
@@ -15,144 +78,5 @@ function convertTemp (input, inputUnit = 'F', outputUnit = 'C') {
 		return undefined;
 	}
 	
-	switch (inputU) {
-		case 'C':
-			if (outputU === 'DE') {
-				return (100 - input) * 3/2;
-			} else if (outputU === 'F') {
-				return (input * 9/5) + 32;
-			} else if (outputU === 'K') {
-				return input + 273.15;
-			} else if (outputU === 'N') {
-				return input * 33/100;
-			} else if (outputU === 'R') {
-				return (input + 273.15) * 9/5;
-			} else if (outputU === 'RE') {
-				return input * 4/5;
-			} else if (outputU === 'RO') {
-				return input * 21/40 + 7.5;
-			}
-			break;
-		case 'DE':
-			if (outputU === 'C') {
-				return 100 - input * 2/3;
-			} else if (outputU === 'F') {
-				return 212 - input * 6/5;
-			} else if (outputU === 'K') {
-				return 373.15 - input * 2/3;
-			} else if (outputU === 'N') {
-				return 33 - input * 11/50;
-			} else if (outputU === 'R') {
-				return 671.67 - input * 6/5;
-			} else if (outputU === 'RE') {
-				return 80 - input * 8/15;
-			} else if (outputU === 'RO') {
-				return 60 - input * 7/20;
-			}
-			break;
-			
-		case 'F':
-			if (outputU === 'C') {
-				return (input - 32) * 5/9;
-			} else if (outputU === 'DE') {
-				return (212 - input) * 5/6;
-			} else if (outputU === 'K') {
-				return (input + 459.67) * 5/9;
-			} else if (outputU === 'N') {
-				return (input - 32) * 11/60;
-			} else if (outputU === 'R') {
-				return input + 459.67;
-			} else if (outputU === 'RE') {
-				return (input - 32) * 4/9;
-			} else if (outputU === 'RO') {
-				return (input - 32) * 7/24 + 7.5;
-			}
-			break;
-		case 'K':
-			if (outputU === 'C') {
-				return input - 273.15;
-			} else if (outputU === 'DE') {
-				return (373.15 - input) * 3/2;
-			} else if (outputU === 'F') {
-				return input * 9/5 - 459.67;
-			} else if (outputU === 'N') {
-				return (input - 273.15) * 33/100;
-			} else if (outputU === 'R') {
-				return input * 9/5;
-			} else if (outputU === 'RE') {
-				return (input - 273.15) * 4/5;
-			} else if (outputU === 'RO') {
-				return (input - 273.15) * 21/40 + 7.5;
-			}
-			break;
-			
-		case 'N':
-			if (outputU === 'C') {
-				return input * 100/33;
-			} else if (outputU === 'DE') {
-				return (33 - input) * 50/11;
-			} else if (outputU === 'F') {
-				return input * 60/11 + 32;
-			} else if (outputU === 'K') {
-				return input * 100/33 + 273.15;
-			} else if (outputU === 'R') {
-				return input * 60/11 + 491.67;
-			} else if (outputU === 'RE') {
-				return input * 80/33;
-			} else if (outputU === 'RO') {
-				return input * 35/22 + 7.5;
-			}
-			break;
-		case 'R':
-			if (outputU === 'C') {
-				return (input - 491.67) * 5/9;
-			} else if (outputU === 'DE') {
-				return (671.67 - input) * 5/6;
-			} else if (outputU === 'F') {
-				return input - 459.67;
-			} else if (outputU === 'K') {
-				return input * 5/9;
-			} else if (outputU === 'N') {
-				return (input - 491.67) * 11/60;
-			} else if (outputU === 'RE') {
-				return (input - 491.67) * 4/9;
-			} else if (outputU === 'RO') {
-				return (input - 491.67) * 7/24 + 7.5;
-			}
-			break;
-		case 'RE':
-			if (outputU === 'C') {
-				return input * 5/4;
-			} else if (outputU === 'DE') {
-				return (80 - input) * 15/8;
-			} else if (outputU === 'F') {
-				return input * 9/4 + 32;
-			} else if (outputU === 'K') {
-				return input * 5/4 + 273.15;
-			} else if (outputU === 'N') {
-				return input * 33/80;
-			} else if (outputU === 'R') {
-				return input * 9/4 + 491.67;
-			} else if (outputU === 'RO') {
-				return input * 21/32 + 7.5;
-			}
-			break;
-		case 'RO':
-			if (outputU === 'C') {
-				return (input - 7.5) * 40/21;
-			} else if (outputU === 'DE') {
-				return (60 - input) * 20/7;
-			} else if (outputU === 'F') {
-				return (input - 7.5) * 24/7 + 32;
-			} else if (outputU === 'K') {
-				return (input - 7.5) * 40/21 + 273.15;
-			} else if (outputU === 'N') {
-				return (input - 7.5) * 22/35;
-			} else if (outputU === 'R') {
-				return (input - 7.5) * 24/7 + 491.67;
-			} else if (outputU === 'RE') {
-				return (input - 7.5) * 32/21;
-			}
-			break;
-	}
+	return getTemp(input, convertCode);
 }
